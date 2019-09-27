7c5331ef503f773a94751f856d715dd8fe57d37b
raymerorale <45501539+raymerorale@users.noreply.github.com>
2019-09-27 08:27:13 +0800

    Add files via upload

---
+++ Todo List/assets/js/todolist.js
@@ -0,0 +1,23 @@
+﻿
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+$("input[type='text'").keypress(function (event) {
+    if (event.which === 13) {
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + $(this).val() + "</li>");
+        $(this).val("");
+    }
+});
+$("#plusminus").click(function () {
+    $("input").toggleClass("show");
+    if($(this).attr("class") === "fa fa-plus")
+        $(this).attr("class", "fa fa-minus");
+    else
+        $(this).attr("class", "fa fa-plus");
+});
